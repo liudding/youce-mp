12ff20bca39c8031acb278093174cee69393752a
liuding <liuding.ld@foxmail.com>
2023-02-02 18:32:53 +0800

    rename answer to result

---
+++ pages/result/result.js
@@ -0,0 +1,27 @@
+// pages/answer/answer.js
+Page({
+
+  data: {
+    questions: [{
+      label: "1",
+      bubbles: [0, 1, 0,0,0],
+      score: 1
+    }, {
+      label: "1",
+      bubbles: [0, 0, 1,0,0],
+      score: 1
+    }, {
+      label: "1",
+      bubbles: [0, 1, 0,0,0],
+      score: 1
+    }]
+  },
+
+  onLoad(options) {
+
+  },
+
+  onShareAppMessage() {
+
+  }
+})
